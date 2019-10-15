f052bf93fdb0ab141ce320d1a7991681b7c5925e
mortal <seregagl1996@gmail.com>
2019-10-15 14:31:38 +0300

    Еще порция доработок

--- js/1.js
+++ js/1.js
@@ -5,6 +5,25 @@ $('.owl-carousel').owlCarousel({
     navText: ['<i class="fas fa-caret-left fa-2x" aria-hidden="true"></i>',
               '<i class="fas fa-caret-right fa-2x" aria-hidden="true"></i>'],
     items: 3,
+    margin: 20,
     loop: true,
     nav: true,
+    center: true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        // breakpoint from 480 up
+        480: {
+            items: 1
+        },
+        // breakpoint from 768 up
+        768: {
+            items: 3
+        },
+        1000: {
+            items: 3
+        }
+    }
 })
