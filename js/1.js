dbedbb041c760402b6a9dd44b96cb1bc3c46d477
mortal <seregagl1996@gmail.com>
2019-10-15 12:57:49 +0300

    Добавил карусель

---
+++ js/1.js
@@ -0,0 +1,10 @@
+$('.owl-carousel').owlCarousel({
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    navText: ['<i class="fas fa-caret-left fa-2x" aria-hidden="true"></i>',
+              '<i class="fas fa-caret-right fa-2x" aria-hidden="true"></i>'],
+    items: 3,
+    loop: true,
+    nav: true,
+})
