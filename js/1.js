631f7b024f98e4e21eab5ce0203289a9ed5bbe8a
mortalshot <48853493+mortalshot@users.noreply.github.com>
2019-11-19 22:19:31 +0300

    Add files via upload

--- js/1.js
+++ js/1.js
@@ -38,25 +38,34 @@ $(document).ready(function () {
 
 });
 
-//smooth scroll
-$(document).ready(function () {
-    $("#menu").on("click", "a", function (event) {
-        event.preventDefault();
-        var id = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({
-            scrollTop: top
-        }, 1300);
-    });
-});
-
 //findWork varying content
 $('#findWork').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var recipient = button.data('whatever') // Extract info from data-* attributes
-  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-  var modal = $(this)
-  modal.find('.modal-title').text('Вакансия ' + recipient)
-  modal.find('.modal-body #recipient-name').val(recipient)
-})
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('Вакансия ' + recipient)
+    modal.find('.modal-body #recipient-name').val(recipient)
+});
+
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
